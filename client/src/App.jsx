fd303901135ccb7f55d1e94f0024a4aacedb235a
Basavaraj Malatesh Mannangi <basavaraj.mannangi@criodo.com>
2024-01-18 13:52:50 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
 function App() {
 
 
   return (
-    <>
-      <h1 className="text-red-700 text-3xl font-bold">Blog App Modified</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,8 @@
+
+const SignUp = () => {
+  return (
+    <div>Signout</div>
+  )
+}
+
+export default SignUp
