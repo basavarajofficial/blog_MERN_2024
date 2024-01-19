bd4e5d6a60bf329162979452c89f7690abb9fec6
Basavaraj Malatesh Mannangi <basavaraj.mannangi@criodo.com>
2024-01-19 09:09:39 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO)
+.then(() => console.log("MOngoDB Connected"))
+.catch((err) => console.log(err.message));
+
 
 app.listen(3000, () => console.log("App listening on port 3000"));
