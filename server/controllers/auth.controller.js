8d263c16be4823d7ed4f9df5749d7ea512e93f24
Basavaraj Malatesh Mannangi <basavaraj.mannangi@criodo.com>
2024-01-19 15:33:58 +0530

    error handler middleware

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,23 +1,24 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username.length === "" || email.length === "" || password.length === ""){
-        return res.status(400).json({message: "all fields required!"});
+        next(errorHandler(400, "All fields are required!"));
     }
 
     const existingUsername = await User.findOne({username});
     const existingUser = await User.findOne({email});
 
     if(existingUsername){
-        return res.status(400).json({message : "username already exists in our database!"});
+        next(errorHandler(400, "username already exists in our database!"));
     }
 
     if(existingUser){
-        return res.status(400).json({message : "email already exists in our database!"});
+        next(errorHandler(400, "email already exists in our database!"));
     }
     
 
@@ -33,6 +34,6 @@ export const signup = async (req, res, next) => {
         await newUser.save();
         res.status(200).json({message: "signup successfully!"});
     } catch (error) {
-        res.status(500).json({message:error.message});
+        next(error)
     }
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import { error } from "console";
 
 dotenv.config();
 
@@ -15,7 +16,19 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => console.log(err.message));
 
 
+
+
 app.listen(3000, () => console.log("App listening on port 3000"));
 
 app.use( '/api/user' , userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message,
+    });
+})
