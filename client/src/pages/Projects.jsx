37253e939623b1fdfa8eb2b1121dffe7ee72aa21
Basavaraj Malatesh Mannangi <basavaraj.mannangi@criodo.com>
2024-02-12 00:37:20 +0530

    finished

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div className="max-w-6xl mx-auto mb-10">
        <div className="bg-orange-400 dark:bg-blue-800 text-3xl lg:text-4xl text-slate-200 m-4 flex justify-center items-center md:gap-10 rounded-lg lg:p-28 p-4 sm:p-10 shadow-lg  mb-16 mt-14 md:h-[65vh]">
-        <h1 className="font-semibold">Projects Will be adding soon</h1>
+        <h1 className="font-semibold">Projects will be adding soon</h1>
         <HiArrowRight className='' />
       </div>
     </div>
